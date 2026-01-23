5f91139e67b390025a98b0486eb3506390204fea
Hein <u6632106@au.edu>
2026-01-23 15:38:52 +0700

    Mongo

--- app/api/hello/route.js
+++ app/api/hello/route.js
@@ -1,7 +1,16 @@
+import corsHeaders from "@/lib/cors";
 import { NextResponse } from "next/server";
 
-export async function GET() {
-  return NextResponse.json({
-    message: "hello world",
+export async function OPTIONS() {
+  return new NextResponse(null, {
+    status: 200,
+    headers: corsHeaders,
   });
+}
+
+export async function GET() {
+  return NextResponse.json(
+    { message: "hello world" },
+    { headers: corsHeaders }
+  );
 }
