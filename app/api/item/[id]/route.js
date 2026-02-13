9b089bd2076ebbad67d98e94723d8fee4c195080
Hein <u6632106@au.edu>
2026-02-13 14:38:12 +0700

    another routejs

--- app/api/item/[id]/route.js
+++ app/api/item/[id]/route.js
@@ -3,18 +3,56 @@ import { getClientPromise } from "@/lib/mongodb";
 import { NextResponse } from "next/server";
 import { ObjectId } from "mongodb";
 
+export async function OPTIONS() {
+  return new Response(null, {
+    status: 200,
+    headers: corsHeaders
+  });
+}
+
 export async function GET(req, { params }) {
+  const { id } = params;
+
   try {
     const client = await getClientPromise();
     const db = client.db("wad-01");
 
-    const item = await db.collection("item").findOne({
-      _id: new ObjectId(params.id),
-    });
+    const result = await db
+      .collection("item")
+      .findOne({ _id: new ObjectId(id) });
 
-    return NextResponse.json(item, {
-      headers: corsHeaders,
-    });
+    return NextResponse.json(result, { headers: corsHeaders });
+  } catch (err) {
+    return NextResponse.json(
+      { message: err.toString() },
+      { status: 400, headers: corsHeaders }
+    );
+  }
+}
+
+export async function PATCH(req, { params }) {
+  const { id } = params;
+  const data = await req.json();
+
+  try {
+    const client = await getClientPromise();
+    const db = client.db("wad-01");
+
+    await db.collection("item").updateOne(
+      { _id: new ObjectId(id) },
+      {
+        $set: {
+          itemName: data.name,
+          itemCategory: data.category,
+          itemPrice: data.price
+        }
+      }
+    );
+
+    return NextResponse.json(
+      { message: "Updated" },
+      { status: 200, headers: corsHeaders }
+    );
   } catch (err) {
     return NextResponse.json(
       { message: err.toString() },
@@ -24,48 +62,19 @@ export async function GET(req, { params }) {
 }
 
 export async function DELETE(req, { params }) {
+  const { id } = params;
+
   try {
     const client = await getClientPromise();
     const db = client.db("wad-01");
 
     await db.collection("item").deleteOne({
-      _id: new ObjectId(params.id),
+      _id: new ObjectId(id)
     });
 
     return NextResponse.json(
-      { success: true },
-      { headers: corsHeaders }
-    );
-  } catch (err) {
-    return NextResponse.json(
-      { message: err.toString() },
-      { status: 400, headers: corsHeaders }
-    );
-  }
-}
-
-export async function PUT(req, { params }) {
-  const data = await req.json();
-
-  try {
-    const client = await getClientPromise();
-    const db = client.db("wad-01");
-
-    await db.collection("item").updateOne(
-      { _id: new ObjectId(params.id) },
-      {
-        $set: {
-          itemName: data.name,
-          itemCategory: data.category,
-          itemPrice: data.price,
-          status: data.status,
-        },
-      }
-    );
-
-    return NextResponse.json(
-      { success: true },
-      { headers: corsHeaders }
+      { message: "Deleted" },
+      { status: 200, headers: corsHeaders }
     );
   } catch (err) {
     return NextResponse.json(
