05ea49de77ffeb9f80a794d3adb634988e001ffc
Hein <u6632106@au.edu>
2026-02-13 14:37:17 +0700

    routejs and corsjs

--- app/api/item/route.js
+++ app/api/item/route.js
@@ -5,19 +5,33 @@ import { NextResponse } from "next/server";
 export async function OPTIONS() {
   return new Response(null, {
     status: 200,
-    headers: corsHeaders,
+    headers: corsHeaders
   });
 }
 
-export async function GET() {
+export async function GET(req) {
+  const headers = {
+    "Cache-Control": "no-store",
+    ...corsHeaders
+  };
+
+  const { searchParams } = new URL(req.url);
+  const page = parseInt(searchParams.get("page")) || 1;
+  const limit = 5;
+  const skip = (page - 1) * limit;
+
   try {
     const client = await getClientPromise();
     const db = client.db("wad-01");
-    const result = await db.collection("item").find({}).toArray();
 
-    return NextResponse.json(result, {
-      headers: corsHeaders,
-    });
+    const result = await db
+      .collection("item")
+      .find({})
+      .skip(skip)
+      .limit(limit)
+      .toArray();
+
+    return NextResponse.json(result, { headers });
   } catch (err) {
     return NextResponse.json(
       { message: err.toString() },
@@ -37,7 +51,7 @@ export async function POST(req) {
       itemName: data.name,
       itemCategory: data.category,
       itemPrice: data.price,
-      status: "ACTIVE",
+      status: "ACTIVE"
     });
 
     return NextResponse.json(
